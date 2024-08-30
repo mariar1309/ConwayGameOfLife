9e0f162903cc88ace3be96b1afc3d69d3b1a26f8
Maria Rufova <mariairufova@gmail.com>
2024-08-30 09:26:36 -0700

    random distribution of cells and preset glider

--- main.py
+++ main.py
@@ -10,3 +10,16 @@ import matplotlib.animation as animation
 #Represent the grid. 255 for ON, 0 for OFF
 x = np.array(([0, 0, 255], [255, 255, 0], [0, 255, 0]))
 plt.imshow(x, interpolation = 'nearest') #'nearst' to get the sharpest edges of cells
+plt.show()
+print ("done")
+
+#random distribution of cells
+np.random.choice([0, 255], 4*4, p=[0.5, 0.5]).reshape(4, 4)
+
+#Instead of random distribution, can set a pattern by initializing 2D grid to zeros and then replacing particular pixels in chosen rows and cols
+def addGlider(i, j, grid):
+    #add a glider with top left cells at (i, j)
+    glider = np.array([[0, 0, 225],[225, 0, 225],[0, 225, 225]])
+    grid[i:i+3, j:j+3]=glider
+grid = np.zeros(10*10).reshape(10, 10)
+addGlider(1, 1, grid)
